72b096929765ac3078c1c28c067c6045f88036b6
Manidom Nadine <tchouamonadine4@gmail.com>
2024-04-21 16:42:50 +0200

    add function All

--- ProduitManager.php
+++ ProduitManager.php
@@ -13,7 +13,7 @@ class ProduitManager
 
 	public function ajouter(Produit $prod) : void
 	{
-		$sql = "INSERT INTO produits (id, nom, prix) VALUES ($prod->id,'$prod->nom', $prod->prix)";
+		$sql = "INSERT INTO produits (nom, prix) VALUES ('$prod->nom', $prod->prix)";
 
 		try 
 		{
@@ -28,9 +28,9 @@ class ProduitManager
 
 	public function recuperer(int $id) : ?Produit
 	{
-		$sql = "SELECT * FROM produits WHERE id = $id LIMIT 1";
+	 	$sql = "SELECT * FROM produits WHERE id = $id LIMIT 1";
 
-		try {
+	 	try {
 			$stmt = $this->database->query($sql);
 			if ($stmt->rowCount() > 0) 
 			{
@@ -46,5 +46,28 @@ class ProduitManager
 
 		return new Produit($data['id'], $data['nom'], $data['prix']);
 
+	}
+
+	public function All()
+	{
+		$sql= " SELECT * FROM produits";
+		try 
+		{
+			$stmt = $this->database->query($sql);
+			$data =$stmt->fetchAll(PDO::FETCH_ASSOC);
+				foreach ($data as $produit)
+				{
+					echo "<tr>";
+					echo "<td>".$produit['id']."</td>";
+					echo "<td>".$produit['nom']."</td>";
+					echo "<td>".$produit['prix']."</td>";
+					echo "</tr>";
+				}
+		} 
+		catch(Exception $e)
+			{
+				$e->getMessage();
+
+			}		
 	}
 }
--- index.php
+++ index.php
@@ -7,11 +7,11 @@ $db = new PDO("mysql:host=127.0.0.1;dbname=nado_hydratation", "root", "");
 
 $manager = new ProduitManager($db);
 
-// $prod = new Produit(2, 'iPhone 14 Pro Max', 800000);
+// $prod = new Produit(2,'iPhone 14 Pro Max', 800000);
 
-$prod = $manager->recuperer(3);
-
-var_dump($prod);
-
+// $prod = $manager->recuperer(3);
+// $prod = $manager->recuperer(3);
+// var_dump($prod);
+$manager->All();
 // $manager->ajouter($prod);
 
